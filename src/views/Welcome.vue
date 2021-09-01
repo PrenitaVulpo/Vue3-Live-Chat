00ebd626333259e7ed4ecc7dc82779557ec85420
PrenitaVulpo <guilhermepdl97@protonmail.com>
2021-09-01 14:42:51 -0300

    now on vue3

---
+++ src/views/Welcome.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="welcome container">welcome</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Welcome",
+  setup() {
+    return {};
+  },
+});
+</script>
+<style scoped>
+.welcome {
+  text-align: center;
+  padding: 1.5rem 0;
+}
+</style>
