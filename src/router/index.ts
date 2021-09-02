3b392c33e9c8f67c1aa88cfcf0268efa62ff17e2
PrenitaVulpo <guilhermepdl97@protonmail.com>
2021-09-01 21:39:54 -0300

    add chatroom page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import Welcome from "../views/Welcome.vue";
+import Chatroom from "../views/Chatroom.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -8,13 +9,9 @@ const routes: Array<RouteRecordRaw> = [
     component: Welcome,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: "/chatroom",
+    name: "Chatroom",
+    component: Chatroom,
   },
 ];
 
---
+++ src/views/Chatroom.vue
@@ -0,0 +1,12 @@
+<template>
+  <p>Chatroom</p>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
