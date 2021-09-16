e639dd9686052f4b501898ebc76a22ff851d985f
PrenitaVulpo <guilhermepdl97@protonmail.com>
2021-09-15 23:59:43 -0300

    add auth check

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,13 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "@/assets/main.css";
+import { auth } from "@/firebase/config";
+import { onAuthStateChanged } from "@firebase/auth";
 
-createApp(App).use(router).mount("#app");
+let app: any = null;
+
+onAuthStateChanged(auth, () => {
+  if (!app) {
+    app = createApp(App).use(router).mount("#app");
+  }
+});
